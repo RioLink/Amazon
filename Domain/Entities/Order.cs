3732d86230f63a68dd062954c681a8d377f704d7
B0puc7pe4ko <b0puc7pe4ko@gmail.com>
2026-06-08 19:48:00 +0200

    Add models to Domain
    Added models such CartItem, Category, Order, OrderItem, Product and User. Also added enum OrderStatus

---
+++ Domain/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalAmount { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+        public string UserId { get; set; } = string.Empty;
+        public User User { get; set; } = null!;
+
+        public ICollection<OrderItem> Items { get; set; } = new List<OrderItem>();
+    }
+}
